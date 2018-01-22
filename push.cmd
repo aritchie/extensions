a9123db494347459852c48b7c70b6ee9811f4eef
Allan Ritchie <allan.ritchie@gmail.com>
2018-01-22 17:33:02 -0500

    update scripts

--- push.cmd
+++ push.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-nuget push .\Acr\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
-del .\Acr\bin\Release\*.nupkg
+rem nuget push .\Acr\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
+rem del .\Acr\bin\Release\*.nupkg
 
 nuget push .\Acr.EfCore\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
 del .\Acr.EfCore\bin\Release\*.nupkg
