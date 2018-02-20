21952657b53e9a95b48d3df5f92a7db3dea4ecf0
Allan Ritchie <allan.ritchie@gmail.com>
2018-02-20 11:53:11 -0500

    add myget for later - nuget processing is slow these days

--- push.cmd
+++ push.cmd
@@ -1,11 +1,14 @@
 @echo off
 
 rem nuget push .\Acr\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push .\Acr\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
 rem del .\Acr\bin\Release\*.nupkg
 
 nuget push .\Acr.EfCore\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push .\Acr.EfCore\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
 del .\Acr.EfCore\bin\Release\*.nupkg
 
 rem nuget push .\Acr.XamForms\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push .\Acr.XamForms\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
 rem del .\Acr.XamForms\bin\Release\*.nupkg
 pause
