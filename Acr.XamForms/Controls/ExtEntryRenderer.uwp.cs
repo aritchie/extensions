7530fc51a0e72f49b1014abe81d2297b845aebe4
Allan Ritchie <allan.ritchie@gmail.com>
2018-03-23 09:17:38 -0400

    fix and rev the release

--- Acr.XamForms/Controls/ExtEntryRenderer.uwp.cs
+++ Acr.XamForms/Controls/ExtEntryRenderer.uwp.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.UWP;
-using Xamarin.Forms.PlatformConfiguration;
-using Acr.XamForms.Controls;
+﻿//using System;
+//using Xamarin.Forms;
+//using Xamarin.Forms.Platform.UWP;
+//using Xamarin.Forms.PlatformConfiguration;
+//using Acr.XamForms.Controls;
 
 
-[assembly: ExportRenderer(typeof(ExtEntry), typeof(UwpExtEntryRenderer))]
-namespace Acr.XamForms.Controls
-{
-    public class UwpExtEntryRenderer : EntryRenderer
-    {
-        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
-        {
-            base.OnElementChanged(e);
+//[assembly: ExportRenderer(typeof(ExtEntry), typeof(UwpExtEntryRenderer))]
+//namespace Acr.XamForms.Controls
+//{
+//    public class UwpExtEntryRenderer : EntryRenderer
+//    {
+//        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+//        {
+//            base.OnElementChanged(e);
 
-            if (this.Control != null && this.Element is ExtEntry entry)
-            {
-                this.Control.KeyDown += (object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs eventArgs) =>
-                {
-                    if (eventArgs.Key == Windows.System.VirtualKey.Enter)
-                    {
-                        entry.InvokeCompleted();
-                        // Make sure to set the Handled to true, otherwise the RoutedEvent might fire twice
-                        eventArgs.Handled = true;
-                    }
-                };
-            }
-        }
-    }
-}
+//            if (this.Control != null && this.Element is ExtEntry entry)
+//            {
+//                this.Control.KeyDown += (object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs eventArgs) =>
+//                {
+//                    if (eventArgs.Key == Windows.System.VirtualKey.Enter)
+//                    {
+//                        entry.InvokeCompleted();
+//                        // Make sure to set the Handled to true, otherwise the RoutedEvent might fire twice
+//                        eventArgs.Handled = true;
+//                    }
+//                };
+//            }
+//        }
+//    }
+//}
--- push.cmd
+++ push.cmd
@@ -4,11 +4,11 @@ rem nuget push .\Acr\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/pa
 rem nuget push .\Acr\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
 rem del .\Acr\bin\Release\*.nupkg
 
-nuget push .\Acr.EfCore\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push .\Acr.EfCore\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
-del .\Acr.EfCore\bin\Release\*.nupkg
+rem nuget push .\Acr.EfCore\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push .\Acr.EfCore\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
+rem del .\Acr.EfCore\bin\Release\*.nupkg
 
-rem nuget push .\Acr.XamForms\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
-rem nuget push .\Acr.XamForms\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
-rem del .\Acr.XamForms\bin\Release\*.nupkg
+nuget push .\Acr.XamForms\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push .\Acr.XamForms\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
+del .\Acr.XamForms\bin\Release\*.nupkg
 pause
