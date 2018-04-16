3e8cdfd44102cbbee9515b051d9814ad9a56e7a1
Allan Ritchie <allan.ritchie@gmail.com>
2018-04-16 10:06:16 -0400

    minor

--- Samples/Samples/PicturePage.xaml.cs
+++ Samples/Samples/PicturePage.xaml.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+using Acr.XamForms.Pages;
 using Xamarin.Forms.Xaml;
+
 
 namespace Samples
 {
