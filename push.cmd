d91ea3cc69f1f4a303ba157b1e1a4510dbc00225
Allan Ritchie <aritchie@users.noreply.github.com>
2018-07-05 13:09:35 -0400

    version bumps

--- push.cmd
+++ push.cmd
@@ -1,14 +1,14 @@
 @echo off
 
 nuget push .\Acr\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push .\Acr\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
+rem nuget push .\Acr\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
 del .\Acr\bin\Release\*.nupkg
 
-rem nuget push .\Acr.EfCore\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push .\Acr.EfCore\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
 rem nuget push .\Acr.EfCore\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
-rem del .\Acr.EfCore\bin\Release\*.nupkg
+del .\Acr.EfCore\bin\Release\*.nupkg
 
 nuget push .\Acr.XamForms\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push .\Acr.XamForms\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
+rem nuget push .\Acr.XamForms\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
 del .\Acr.XamForms\bin\Release\*.nupkg
 pause
