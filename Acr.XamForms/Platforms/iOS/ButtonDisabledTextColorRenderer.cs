28b66e4e4fb3c6f2bbb9a8bdbc64daa8876d4931
Allan Ritchie <allan.ritchie@gmail.com>
2018-10-05 21:34:54 -0400

    cleanup

--- Acr.XamForms/Platforms/iOS/ButtonDisabledTextColorRenderer.cs
+++ Acr.XamForms/Platforms/iOS/ButtonDisabledTextColorRenderer.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using Foundation;
-using UIKit;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.iOS;
-using Acr.XamForms.Controls;
+﻿//using System;
+//using Foundation;
+//using UIKit;
+//using Xamarin.Forms;
+//using Xamarin.Forms.Platform.iOS;
+//using Acr.XamForms.Controls;
 
 
-[assembly: ExportRenderer(typeof(Button), typeof(ButtonDisabledTextColorRenderer))]
-namespace Acr.XamForms.Controls
-{
-    [Preserve(AllMembers = true)]
-    public class ButtonDisabledTextColorRenderer : ButtonRenderer
-    {
-        static UIColor DisabledColor => Color.FromHex("#faebd7").ToUIColor();
+//[assembly: ExportRenderer(typeof(Button), typeof(ButtonDisabledTextColorRenderer))]
+//namespace Acr.XamForms.Controls
+//{
+//    [Preserve(AllMembers = true)]
+//    public class ButtonDisabledTextColorRenderer : ButtonRenderer
+//    {
+//        static UIColor DisabledColor => Color.FromHex("#faebd7").ToUIColor();
 
 
-        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            base.OnElementPropertyChanged(sender, e);
-            if (e.PropertyName == nameof(Button.IsEnabled))
-                this.Element.Opacity = this.Element.IsEnabled ? 1 : .5;
-        }
+//        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+//        {
+//            base.OnElementPropertyChanged(sender, e);
+//            if (e.PropertyName == nameof(Button.IsEnabled))
+//                this.Element.Opacity = this.Element.IsEnabled ? 1 : .5;
+//        }
 
 
-        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
-        {
-            base.OnElementChanged(e);
-            if (this.Control == null)
-                return;
+//        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+//        {
+//            base.OnElementChanged(e);
+//            if (this.Control == null)
+//                return;
 
-            var title = new NSAttributedString(Control.CurrentTitle, new UIStringAttributes { ForegroundColor = DisabledColor });
-            this.Control.SetAttributedTitle(title, UIControlState.Disabled);
-        }
-    }
-}
+//            var title = new NSAttributedString(Control.CurrentTitle, new UIStringAttributes { ForegroundColor = DisabledColor });
+//            this.Control.SetAttributedTitle(title, UIControlState.Disabled);
+//        }
+//    }
+//}
