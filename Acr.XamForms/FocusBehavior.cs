ee3154c1bced1a4a3e3abdf4e5c72918238c36dc
Allan Ritchie <allan.ritchie@gmail.com>
2018-10-08 10:51:31 -0400

    bindable focus behavior

---
+++ Acr.XamForms/FocusBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xamarin.Forms;
+
+
+namespace Acr.XamForms
+{
+    public class FocusBehavior : AbstractBindableBehavior<View>
+    {
+        protected override void OnAttachedTo(View view)
+        {
+            view.FocusChangeRequested += this.OnFocusChangeRequested;
+            base.OnAttachedTo(view);
+        }
+
+
+        protected override void OnDetachingFrom(View view)
+        {
+            view.FocusChangeRequested -= this.OnFocusChangeRequested;
+            base.OnDetachingFrom(view);
+        }
+
+
+        public static readonly BindableProperty IsFocusedProperty = BindableProperty.Create(
+            nameof(IsFocused),
+            typeof(string),
+            typeof(FocusBehavior),
+            null,
+            propertyChanged: OnPropertyChanged
+        );
+        public bool IsFocused
+        {
+            get => (bool) this.GetValue(IsFocusedProperty);
+            set => this.SetValue(IsFocusedProperty, value);
+        }
+
+
+        static void OnPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var behavior = (FocusBehavior)bindable;
+            if (behavior.AssociatedObject == null)
+                return;
+
+            var changed = behavior.AssociatedObject.IsFocused != (bool) newValue;
+            if (!changed)
+                return;
+
+            if ((bool) newValue)
+            {
+                behavior.AssociatedObject.Focus();
+            }
+            else
+            {
+                behavior.AssociatedObject.Unfocus();
+            }
+        }
+
+
+        void OnFocusChangeRequested(object sender, VisualElement.FocusRequestArgs e)
+            => this.IsFocused = e.Focus;
+    }
+}
